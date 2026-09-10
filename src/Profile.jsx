1cf58e8459e97ef16eb2ce4aee36b0229b8023ab
Clyde Chiruka <clydechiruka4@gmail.com>
2026-09-10 15:11:49 +0200

    Fix import casing for UseBackClose

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,7 +6,7 @@ import { useTheme } from './ThemeContext'
 import { usePushNotifications } from './usePushNotifications'
 import { getDisplayName } from './DisplayName'
 import ReportsScreen from './ReportsScreen'
-import { useBackClose } from './usebackClose'
+import { useBackClose } from './UseBackClose'
 
 const DEPARTMENTS = [
   'Accountancy',
